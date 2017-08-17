d56f8fccf60dfbdbbd3decdbbd1ad2b450747f77
billwillman <billwillman@qq.com>
2017-08-17 15:06:46 +0800

    [增加]UI框架

---
+++ UITabView/Assets/Controls/UI/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using NsLib.UI.Message;
+
+namespace NsLib.UI {
+
+    using HWND = String;
+    using LRESULT = Int64;
+
+    // UI管理器, 可以发送消息 SendMsg和PostMsg
+    public sealed class UIManager: MsgCenter {
+
+        // 打开界面
+        
+
+        /* ---------Static区域--------- */
+
+        public static UIManager GetInstance() {
+            if (m_Mgr == null) {
+                GameObject gameObj = new GameObject("UIManager");
+                m_Mgr = gameObj.AddComponent<UIManager>();
+                
+            }
+            return m_Mgr;
+        }
+
+        public static UIManager Instance {
+            get {
+                return GetInstance();
+            }
+        }
+
+        private static UIManager m_Mgr = null;
+        /*---------------------*/
+    }
+}
