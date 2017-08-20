8ddf961d1ceec747529d7235bb38f8bbe48501c5
amumu <345036769@qq.com>
2017-08-20 17:14:18 +0800

    【增加】聊天框实例

---
+++ UITabView/Assets/Chat/ChatDataManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ChatData
+{
+    public string Icon {
+        get;
+        set;
+    }
+
+    public string Text {
+        get;
+        set;
+    }
+
+    public string Name {
+        get;
+        set;
+    }
+}
+
+public class ChatDataManager
+{
+
+    public ChatDataManager()
+    {
+
+    }
+
+    public ChatData GetChatData(int index)
+    {
+        if (index < 0 || index >= m_ChatList.Count)
+            return null;
+        return m_ChatList [index];
+    }
+
+    public void AddChat(string icon, string text, string name)
+    {
+        ChatData chat = new ChatData ();
+        chat.Icon = icon;
+        chat.Text = text;
+        chat.Name = name;
+        m_ChatList.Add (chat);
+    }
+
+    public int ChatCount
+    {
+        get {
+            return m_ChatList.Count;
+        }
+    }
+
+    private List<ChatData> m_ChatList = new List<ChatData> ();
+
+
+    public static ChatDataManager GetInstance()
+    {
+        if (m_Mgr == null)
+            m_Mgr = new ChatDataManager ();
+        return m_Mgr;
+    }
+
+    public static ChatDataManager Instance
+    {
+        get {
+            return GetInstance ();
+        }
+    }
+
+    private static ChatDataManager m_Mgr = null;
+}
---
+++ UITabView/Assets/Chat/WndChat.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WndChat : MonoBehaviour, ITableViewData {
+
+    private UITableView m_ChatView = null;
+    private UIInput m_InputView = null;
+
+    void Start()
+    {
+        var trans = this.transform;
+        m_ChatView = trans.FindChild ("ChatRoot/ChatView").GetComponent<UITableView>();
+        m_ChatView.Data = this;
+        m_ChatView.InitItemCount (0);
+
+        m_InputView = trans.FindChild ("ChatRoot/InputRoot").GetComponent<UIInput> ();
+    }
+
+    public void OnTabViewData (int index, UIWidget item, int subIndex)
+    {
+        if (subIndex == 0) {
+            ChatData data = ChatDataManager.Instance.GetChatData (index);
+            if (data == null)
+                return;
+            var trans = item.cachedTransform;
+            UILabel lb = trans.FindChild ("Label").GetComponent<UILabel> ();
+            lb.text = data.Text;
+        }
+    }
+
+    public void OnTabViewItemSize (int index, UIWidget item)
+    {
+       // Debug.Log ("ItemSize");
+        if (ChatDataManager.Instance.ChatCount <= 0)
+            return;
+        
+        var trans = item.cachedTransform;
+        var lb = trans.FindChild ("Label").GetComponent<UILabel>();
+        int height = (int)(lb.height + lb.cachedTransform.localPosition.y);
+        if (height < 100)
+            item.height = 100;
+        else
+            item.height = height;
+    }
+
+    public void OnBtnSendClick()
+    {
+        // 点击事件
+        string text = m_InputView.value;
+        if (string.IsNullOrEmpty (text))
+            return;
+        
+        ChatDataManager.Instance.AddChat (string.Empty, text, "123");
+        m_ChatView.AddItem ();
+    }
+}
