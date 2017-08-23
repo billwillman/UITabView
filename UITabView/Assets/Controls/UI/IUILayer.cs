f322a680553dc06329d5d588edd3e9871707d3c5
billwillman <billwillman@qq.com>
2017-08-23 20:22:50 +0800

    [修改]UI层级

---
+++ UITabView/Assets/Controls/UI/IUILayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NsLib.UI {
+    
+    // UI层级关系
+    public enum UILayerType {
+        // 主面板层级=>会和主界面同层，关闭则都关闭
+        UILayer_MainWindow = 0,
+        // 普通界面层，普通界面和主界面以及其他普通界面互斥，会关闭
+        UILayer_NormalWindow = 1,
+        // 弹框层，可覆盖在普通界面以及主界面上，不会关闭其他界面
+        UILayer_PopupWindow = 2
+    }
+
+    // UI层级
+    public interface IUILayer {
+
+    }
+}
